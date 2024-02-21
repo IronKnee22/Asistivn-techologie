bf568d9a115b6714996a35708f9018012bc2ec59
IronKnee <marekdarsa@gmail.com>
2024-02-21 15:41:54 +0100

    musi byt spatně diferencialni rovnice

--- cv1/asistivní technologie - kody/borec.m
+++ cv1/asistivní technologie - kody/borec.m
@@ -1,13 +1,36 @@
 clear
 load('EMG.txt')
 
-A=[EMG(:,1) EMG(:,2)]
-vstupy = abs(A(1:100,2));
-vystupy = zeros(size(vstupy)); % Inicializace výstupního pole
+A = [EMG(:,1) abs(EMG(:,2))];
+plot(A(:,2))
 
-for i = 4:vstupy
-    vystupy(i) = 0.9289 * vystupy(i-1) + 0.9976 * vystupy(i-2) - 0.8698 * vystupy(i-3) ... 
-        + 0.00006096 * vstupy(i) + 0.0018 * vstupy(i-1) + 0.0018 * vstupy(i-2) + 0.00006096 * vstupy(i-3);
+vystupy = zeros(size(A,1), 1);
+
+uk1 = 0;
+uk2 = 0;
+uk3 = 0;
+
+yk1 = 0;
+yk2 = 0;
+yk3 = 0;
+
+for i = 1:size(A,1)
+    if i >=2000
+        i = 1
+    end
+    uk = A(i,2);
+    yk = 0.9289*yk1 + 0.9976*yk2 - 0.8698*yk3 + 0.00006096*uk + 0.0018*uk1 + 0.0018*uk2 + 0.00006096*uk3;
+    
+    vystupy(i) = yk;
+
+    uk3 = uk2;
+    uk2 = uk1;
+    uk1 = uk;
+
+    yk3 = yk2;
+    yk2 = yk1;
+    yk1 = yk;
+
 end
 
 figure;
